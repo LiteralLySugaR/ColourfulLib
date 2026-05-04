68abceb4ea2891bd9356e1aba2d4cbde7f4ec3d4
LiteralLySugaR <75783315+LiteralLySugaR@users.noreply.github.com>
2026-05-04 14:45:06 +0300

    Release

---
+++ CLColour.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+namespace ColourfulLib
+{
+    public class CLColour
+    {
+        public byte R { get; private set; } = 0;
+        public byte G { get; private set; } = 0;
+        public byte B { get; private set; } = 0;
+        public byte A { get; private set; } = 255;
+
+        /// <summary>
+        /// Initialise a new colour using RGBA values.
+        /// </summary>
+        /// <param name="r">Red value (0 to 255)</param>
+        /// <param name="g">Green value (0 to 255)</param>
+        /// <param name="b">Blue value (0 to 255)</param>
+        /// <param name="a">Alpha/Transparency value (0 to 255)</param>
+        public CLColour(byte r, byte g, byte b, byte a = 255)
+        {
+            r = (byte)CLMath.Clamp(0, r, 255);
+            g = (byte)CLMath.Clamp(0, g, 255);
+            b = (byte)CLMath.Clamp(0, b, 255);
+            a = (byte)CLMath.Clamp(0, a, 255);
+
+            R = r;
+            G = g;
+            B = b;
+            A = a;
+        }
+        /// <summary>
+        /// Initialise a new colour using RGBA float values.
+        /// </summary>
+        /// <param name="r">Red value (0 to 1)</param>
+        /// <param name="g">Green value (0 to 1)</param>
+        /// <param name="b">Blue value (0 to 1)</param>
+        /// <param name="a">Alpha/Transparency value (0 to 1)</param>
+        public CLColour(float r, float g, float b, float a = 1f)
+        {
+            r = CLMath.Clamp(0, r, 1);
+            g = CLMath.Clamp(0, g, 1);
+            b = CLMath.Clamp(0, b, 1);
+            a = CLMath.Clamp(0, a, 1);
+
+            R = (byte)(255 * r);
+            G = (byte)(255 * g);
+            B = (byte)(255 * b);
+            A = (byte)(255 * a);
+        }
+        /// <summary>
+        /// Initialise a new colour using Microsoft's Color.
+        /// </summary>
+        /// <param name="color">Microsoft colour struct</param>
+        public CLColour(Color color)
+        {
+            R = color.R;
+            G = color.G;
+            B = color.B;
+            A = color.A;
+        }
+        /// <summary>
+        /// Initialise a new colour using hex.
+        /// </summary>
+        /// <param name="hex">Hex value, with or without a hashtag.</param>
+        public CLColour(string hex)
+        {
+            if (hex.StartsWith("#"))
+            {
+                hex.Trim('#');
+            }
+
+            if (hex.Length < 6 || hex.Length % 2 == 1)
+            {
+                R = 0;
+                G = 0;
+                B = 0;
+                A = 0;
+                return;
+            }
+
+            if (hex.Length > 8)
+            {
+                hex.Substring(0, 8);
+            }
+
+            byte[] hexResult = new byte[hex.Length / 2];
+
+            int chunk = 0;
+
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                string hexChunk = hex.Substring(i, 2);
+
+                hexResult[chunk] = Convert.ToByte(hexChunk, 16);
+
+                chunk++;
+            }
+
+            R = (byte)CLMath.Clamp(0, hexResult[0], 255);
+            G = (byte)CLMath.Clamp(0, hexResult[1], 255);
+            B = (byte)CLMath.Clamp(0, hexResult[2], 255);
+            A = 255;
+            if (hexResult.Length > 3)
+            {
+                A = (byte)CLMath.Clamp(0, hexResult[3], 255);
+            }
+        }
+        /* ================================================== */
+        /// <summary>
+        /// Get Colour's RGB.
+        /// </summary>
+        /// <param name="includeAlpha">Should the return array include Alpha/Transparency.</param>
+        /// <returns>Byte array [R,G,B,(A)]</returns>
+        public byte[] GetRGB(bool includeAlpha)
+        {
+            if (includeAlpha)
+            {
+                return new byte[4] { R, G, B, A };
+            }
+            return new byte[3] { R, G, B };
+        }
+        /// <summary>
+        /// Get string of colour's RGB(A).
+        /// </summary>
+        /// <param name="includeAlpha">Should the return array include Alpha/Transparency.</param>
+        /// <returns>String of RGB(A) as rgba(R,G,B,A) or rgb(R,G,B)</returns>
+        public string GetRGBString(bool includeAlpha)
+        {
+            if (includeAlpha)
+            {
+                return $"rgba({R},{G},{B},{A})";
+            }
+            return $"rgb({R},{G},{B})";
+        }
+        /// <summary>
+        /// Get Hex of this colour.
+        /// </summary>
+        /// <param name="withHashtag">Should the return array include hastag.</param>
+        /// <returns>Hex as string.</returns>
+        public string GetHEX(bool withHashtag)
+        {
+            if (withHashtag)
+            {
+                return $"#{R:X2}{G:X2}{B:X2}{A:X2}";
+            }
+            return $"{R:X2}{G:X2}{B:X2}{A:X2}";
+        }
+        /// <summary>
+        /// Get Hex as array of this colour.
+        /// </summary>
+        /// <returns>string array [R,G,B,A] with values as hex.</returns>
+        public string[] GetHEXArray()
+        {
+            return new string[4] { R.ToString("X2"), G.ToString("X2"), B.ToString("X2"), A.ToString("X2") };
+        }
+        /// <summary>
+        /// Get colour HSVA as int array.
+        /// </summary>
+        /// <returns>Int array [H,S,V,A].</returns>
+        public int[] GetHSVA()
+        {
+            float normalisedR = CLMath.NormaliseByte(R);
+            float normalisedG = CLMath.NormaliseByte(G);
+            float normalisedB = CLMath.NormaliseByte(B);
+            float normalisedA = CLMath.NormaliseByte(A);
+
+            float[] rgbArray = new float[3] { normalisedR, normalisedG, normalisedB };
+
+            rgbArray.ToList().Sort();
+
+            float minRGB = rgbArray[0];
+
+            float maxRGB = rgbArray[2];
+
+            float difference = maxRGB - minRGB;
+
+            float value = maxRGB * 100;
+
+            float saturation = 0f;
+            if (maxRGB != 0)
+            {
+                saturation = difference / maxRGB * 100;
+            }
+
+            float hue = 0f;
+            if (maxRGB == normalisedR)
+            {
+                hue = 60 * (((normalisedG - normalisedB) / difference) % 6);
+            }
+            if (maxRGB == normalisedG)
+            {
+                hue = 60 * (((normalisedB - normalisedR) / difference) + 2);
+            }
+            if (maxRGB == normalisedB)
+            {
+                hue = 60 * (((normalisedR - normalisedG) / difference) + 4);
+            }
+
+            int[] HSVA = new int[4]
+            {
+                (int)Math.Round(hue, MidpointRounding.AwayFromZero),
+                (int)Math.Round(saturation, MidpointRounding.AwayFromZero),
+                (int)Math.Round(value, MidpointRounding.AwayFromZero),
+                ((int)Math.Round(normalisedA, MidpointRounding.AwayFromZero) * 100)
+            };
+
+            return HSVA;
+        }
+        /// <summary>
+        /// Convert HSVA to RGBA.
+        /// </summary>
+        /// <param name="hue">Hue value (0 to 360)</param>
+        /// <param name="sat">Saturation value (0 to 100)</param>
+        /// <param name="val">Value value (0 to 100)</param>
+        /// <param name="alp">Alpha/Transparency value (0 to 100)</param>
+        /// <returns>Byte array [R,G,B,A].</returns>
+        public static byte[] HSVAToRGBA(int hue, int sat, int val, int alp = 100)
+        {
+            float h = CLMath.Clamp(0, hue, 360);
+            float s = CLMath.Clamp(0, sat, 100);
+            float v = CLMath.Clamp(0, val, 100);
+            float a = CLMath.Clamp(0, alp, 100);
+
+            s /= 100;
+            v /= 100;
+
+            float chroma = v * s;
+
+            float inter = chroma * (1 - Math.Abs(((h / 60) % 2) - 1));
+
+            float match = v - chroma;
+
+            float intR = 0f;
+            float intG = 0f;
+            float intB = 0f;
+            if (0 <= h && h < 60)
+            {
+                intR = chroma;
+                intG = inter;
+                intB = 0;
+            }
+            if (60 <= h && h < 120)
+            {
+                intR = inter;
+                intG = chroma;
+                intB = 0;
+            }
+            if (120 <= h && h < 180)
+            {
+                intR = 0;
+                intG = chroma;
+                intB = inter;
+            }
+            if (180 <= h && h < 240)
+            {
+                intR = 0;
+                intG = inter;
+                intB = chroma;
+            }
+            if (240 <= h && h < 300)
+            {
+                intR = inter;
+                intG = 0;
+                intB = chroma;
+            }
+            if (300 <= h && h < 360)
+            {
+                intR = chroma;
+                intG = 0;
+                intB = inter;
+            }
+
+            byte[] rgba = new byte[4]
+            {
+                (byte)Math.Round((intR + match) * 255, MidpointRounding.AwayFromZero),
+                (byte)Math.Round((intG + match) * 255, MidpointRounding.AwayFromZero),
+                (byte)Math.Round((intB + match) * 255, MidpointRounding.AwayFromZero),
+                (byte)Math.Round((double)((a / 100) * 255), MidpointRounding.AwayFromZero)
+            };
+
+            return rgba;
+        }
+        /* ================================================== */
+        /// <summary>
+        /// Normalise RGBA colours.
+        /// </summary>
+        /// <returns>Float array [R,G,B,A] with values between 0 and 1.</returns>
+        public float[] NormaliseSelf()
+        {
+            return new float[4] { (R / 255), (G / 255), (B / 255), (A / 255) };
+        }
+        /// <summary>
+        /// Convert to Microsoft's color.
+        /// </summary>
+        /// <returns>Microsoft's color.</returns>
+        public Color ToMSColor()
+        {
+            return Color.FromArgb(A, R, G, B);
+        }
+        /* ================================================== */
+        /// <summary>
+        /// Get Bitmap's average RGBA colour.
+        /// </summary>
+        /// <param name="bmp">Target Bitmap</param>
+        /// <returns>CLColour.</returns>
+        public static CLColour AverageImage(Bitmap bmp)
+        {
+            int[] RgbaSum = new int[4] { 0, 0, 0, 0 };
+
+            int pixelsAmount = bmp.Width * bmp.Height;
+
+            for (int h = 0; h < bmp.Height; h++)
+            {
+                for (int w = 0; w < bmp.Width; w++)
+                {
+                    CLColour pixelColour = new CLColour(bmp.GetPixel(w, h));
+
+                    RgbaSum[0] += pixelColour.R;
+                    RgbaSum[1] += pixelColour.G;
+                    RgbaSum[2] += pixelColour.B;
+                    RgbaSum[3] += pixelColour.A;
+                }
+            }
+
+            byte[] RGBA = new byte[4]
+            { 
+                (byte)(RgbaSum[0] / pixelsAmount), 
+                (byte)(RgbaSum[1] / pixelsAmount), 
+                (byte)(RgbaSum[2] / pixelsAmount), 
+                (byte)(RgbaSum[3] / pixelsAmount)
+            };
+
+            return new CLColour(RGBA[0], RGBA[1], RGBA[2], RGBA[3]);
+        }
+        /// <summary>
+        /// Get Bitmap's average HSVA colour.
+        /// </summary>
+        /// <param name="bmp">Target Bitmap</param>
+        /// <returns>int array [H,S,V,A]</returns>
+        public static int[] AverageImageHSVA(Bitmap bmp)
+        {
+            byte[] RGBA = CLColour.AverageImage(bmp).GetRGB(true);
+
+            return new CLColour(RGBA[0], RGBA[1], RGBA[2], RGBA[3]).GetHSVA();
+        }
+    }
+}
---
+++ CLMath.cs
@@ -0,0 +1,36 @@
+﻿namespace ColourfulLib
+{
+    internal class CLMath
+    {
+        // For some reasons, Microsoft's Math doesnt
+        // include a Clamp function ...
+        internal static float Clamp(float min, float value, float max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            if (value > max)
+            {
+                return max;
+            }
+            return value;
+        }
+        internal static int Clamp(int min, int value, int max)
+        {
+            if (value < min)
+            {
+                return min;
+            }
+            if (value > max)
+            {
+                return max;
+            }
+            return value;
+        }
+        internal static float NormaliseByte(byte value)
+        {
+            return value / 255;
+        }
+    }
+}
